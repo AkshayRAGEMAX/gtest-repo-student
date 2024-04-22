d191b6b1f098c298d34f46fdf6c692a5591a2253
AkshayRAGEMAX <167761091+AkshayRAGEMAX@users.noreply.github.com>
2024-04-22 15:52:51 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -5,16 +5,6 @@ TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
     //Arrange
     StringCalculator objUnderTest;
     string input="";
-    int expectedValue=10;
-    //Act
-   int actualValue=  objUnderTest.Add(input);
-    //Assert
-    ASSERT_EQ(actualValue,expectedValue);
-}
-TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
-    //Arrange
-    StringCalculator objUnderTest;
-    string input="0";
     int expectedValue=0;
     //Act
    int actualValue=  objUnderTest.Add(input);
